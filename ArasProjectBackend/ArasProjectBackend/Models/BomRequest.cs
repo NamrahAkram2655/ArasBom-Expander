9e91cf7c035da73ce9441f918e63226cf1b7a6e9
Namrah Akram <namrah.akram@xavor.com>
2025-07-27 01:14:07 +0500

    Report Updated Compoenent

--- ArasProjectBackend/ArasProjectBackend/Models/BomRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace ArasProjectBackend.Models
-{
-    public class BomRequest
-    {
-        public string PartNumber { get; set; }
-        public int Level { get; set; }
-    }
-}
--- ArasProjectBackend/ArasProjectBackend/Models/SessionStore.cs
+++
@@ -1,9 +0,0 @@
-using Aras.IOM;
-
-namespace ArasProjectBackend.Utils
-{
-    public static class SessionStore
-    {
-        public static Dictionary<string, HttpServerConnection> UserSessions = new();
-    }
-}
